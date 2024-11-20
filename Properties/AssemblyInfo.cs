2cfbaacda7ef70c0cb14c663f67722f65e147140
ImDragonxd07 <microsoftjake101@gmail.com>
2024-11-20 17:14:03 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Il2CppSystem;
+using MelonLoader;
+// ...
+[assembly: MelonInfo(typeof(Daydream.Client.Classes.Daydream), "Daydream", "0.1", "Dragonxd & Ninka_")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonPriority(-100)]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Daydream")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: MelonColor((System.ConsoleColor)ConsoleColor.DarkYellow)]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Daydream")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c66fe81e-4f9b-493a-9307-9acf8fe34564")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
